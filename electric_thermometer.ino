ed85b680e472c192ac18d5a3a475b89b0290b36a
Rakhmatullah Yoga Sutrisna <13512053@std.stei.itb.ac.id>
2015-05-02 20:02:47 +0700

    spek wajib minus serial communication done

--- electric_thermometer.ino
+++ electric_thermometer.ino
@@ -1,12 +1,11 @@
-#include <SoftwareSerial.h>
-
-#define inPinThermo 0
+#define inPinThermo A0
+#define buttonPin 1
 #define data 2
 #define clock 3
-#define second_digit 4
-#define first_digit 5
-
-SoftwareSerial mySerial(10, 11); // RX, TX
+#define firstDigitPin 6
+#define secondDigitPin 5
+#define thirdDigitPin 4
+#define ledPin 7
 
 // use binary notation to discribe our number layouts
 byte zero  = B00010001;
@@ -21,8 +20,11 @@ byte eight = B00000001;
 byte nine  = B00001001;
 byte point = B11111110;
 
-int counter;
 bool overheat;
+int temperatureMode;
+unsigned long previousMillis;
+const long interval = 1000;
+float celcius;
 
 byte toByte(int digit) {
   switch (digit) {
@@ -56,49 +58,134 @@ byte toByte(int digit) {
     case 9:
       return nine;
       break;
+    default:
+      break;
   }
 }
 
-void printSevenSeg(int number) {
-  int puluhan = (number/10)%10;
-  int satuan = number%10;
-  
-  for(int i=0;i<50;i++) {
-    digitalWrite(first_digit, HIGH);
-    digitalWrite(second_digit, LOW);
-    shiftOut(data, clock, LSBFIRST, toByte(puluhan));
-    delay(10);
-    digitalWrite(first_digit, LOW);
-    digitalWrite(second_digit, HIGH);
-    shiftOut(data, clock, LSBFIRST, toByte(satuan));
-    delay(10);
+void printSevenSeg(float number) {
+  int first_digit;
+  int second_digit;
+  int third_digit;
+  if(number<100) {
+    first_digit = ((int)number/10)%10;
+    second_digit = (int)number%10;
+    third_digit = static_cast<int>(number*10)%10;
+    digitalWrite(firstDigitPin, HIGH);
+    digitalWrite(secondDigitPin, LOW);
+    digitalWrite(thirdDigitPin, LOW);
+    shiftOut(data, clock, LSBFIRST, toByte(first_digit));
+    delay(7);
+    digitalWrite(firstDigitPin, LOW);
+    digitalWrite(secondDigitPin, HIGH);
+    digitalWrite(thirdDigitPin, LOW);
+    shiftOut(data, clock, LSBFIRST, (toByte(second_digit)&point));
+    delay(7);
+    digitalWrite(firstDigitPin, LOW);
+    digitalWrite(secondDigitPin, LOW);
+    digitalWrite(thirdDigitPin, HIGH);
+    shiftOut(data, clock, LSBFIRST, toByte(third_digit));
+    delay(7);
   }
+  else {
+    first_digit = ((int)number/100)%10;
+    second_digit = ((int)number/10)%10;
+    third_digit = (int)number%10;
+    digitalWrite(firstDigitPin, HIGH);
+    digitalWrite(secondDigitPin, LOW);
+    digitalWrite(thirdDigitPin, LOW);
+    shiftOut(data, clock, LSBFIRST, toByte(first_digit));
+    delay(7);
+    digitalWrite(firstDigitPin, LOW);
+    digitalWrite(secondDigitPin, HIGH);
+    digitalWrite(thirdDigitPin, LOW);
+    shiftOut(data, clock, LSBFIRST, toByte(second_digit));
+    delay(7);
+    digitalWrite(firstDigitPin, LOW);
+    digitalWrite(secondDigitPin, LOW);
+    digitalWrite(thirdDigitPin, HIGH);
+    shiftOut(data, clock, LSBFIRST, toByte(third_digit));
+    delay(7);
+  }
+}
+
+float convertSuhu(float suhu, int mode) {
+  float conversion;
+  switch(mode) {
+    case 0: // Celcius
+      conversion = suhu;
+      break;
+    case 1: // Reamur
+      conversion = suhu*4/5;
+      break;
+    case 2: // Kelvin
+      conversion = suhu+273;
+      break;
+    case 3: // Fahrenheit
+      conversion = suhu*9/5+32;
+      break;
+    default:
+      conversion = 0;
+      break;
+  }
+  return conversion;
 }
 
 void setup()
 {
   pinMode(clock, OUTPUT);
   pinMode(data , OUTPUT);
-  pinMode(second_digit, OUTPUT);
-  pinMode(first_digit, OUTPUT);
+  pinMode(firstDigitPin, OUTPUT);
+  pinMode(secondDigitPin, OUTPUT);
+  pinMode(thirdDigitPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
   Serial.begin(9600);
   while(!Serial);
-  Serial.println("Goodnight moon!");
-//  mySerial.begin(4800);
-//  mySerial.println("Hello, world?");
-  counter = 0;
+  Serial.println("Hello world!");
+  temperatureMode = 0;
+  delay(1000);
+  previousMillis = 0;
+  int value = analogRead(inPinThermo);
+  float millivolts = (value / 1024.0) * 100;
+  celcius = millivolts * 3.3;
 }
 
-void loop()
-{
-//  if (mySerial.available())
-//    Serial.write(mySerial.read());
-//  if (Serial.available())
-//    mySerial.write(Serial.read());
-  int value = analogRead(inPinThermo);
-  float millivolts = (value / 1024.0) * 5000;
-  int celcius = millivolts / 10;
-  printSevenSeg(celcius);
-  Serial.println("Suhu saat ini: " + celcius); // ini salah, format keluarannya jadi ngaco
+void loop() {
+  unsigned long currentMillis;
+  temperatureMode = temperatureMode%4;
+  while(digitalRead(buttonPin) == HIGH) {
+    currentMillis = millis();
+    if(currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      int value = analogRead(inPinThermo);
+      float millivolts = (value / 1024.0) * 100;
+      celcius = millivolts * 3.3;
+    }
+    printSevenSeg(convertSuhu(celcius,temperatureMode));
+    if(celcius>23.0) {
+      digitalWrite(ledPin, HIGH);
+    }
+    else {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+  while(digitalRead(buttonPin) == LOW) {
+    currentMillis = millis();
+    if(currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      int value = analogRead(inPinThermo);
+      float millivolts = (value / 1024.0) * 100;
+      celcius = millivolts * 3.3;
+    }
+    printSevenSeg(convertSuhu(celcius,temperatureMode));
+    if(celcius>23.0) {
+      digitalWrite(ledPin, HIGH);
+    }
+    else {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+  temperatureMode++;
 }
 
